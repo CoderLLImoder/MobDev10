c9ad5df09ff367acbd66a0bff3f929c4556736ee
CoderLLImoder <kaperskiyi@gmail.com>
2023-06-25 11:46:04 +0300

    New empty project

---
+++ HW10/HW10App.swift
@@ -0,0 +1,17 @@
+//
+//  HW10App.swift
+//  HW10
+//
+//  Created by Илья Капёрский on 24.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct HW10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
